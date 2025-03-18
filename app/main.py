e069c1f6314b929d61cb6b103b91f5ddba849525
110en <alinilf14@gmail.com>
2025-03-18 15:40:57 -0400

    removed extra "answer" header

--- app/main.py
+++ app/main.py
@@ -3,8 +3,6 @@ from QandA import make_vars, get_chain_ans, make_vec_db
 
 st.title("Fun Flower Facts Q&A")
 question = st.text_input("Question: ", value = "Type a question regarding flowers, then press enter")
-st.header("Answer: ")
-
 
 if question:
     llm, embed = make_vars()
